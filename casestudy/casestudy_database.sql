482b50d1b473ae476f9c066ac84f6e3691e9742a
donghg201 <81423785+donghg201@users.noreply.github.com>
2021-10-03 08:27:37 +0700

    Add files via upload

--- casestudy/casestudy_database.sql
+++ casestudy/casestudy_database.sql
@@ -39,6 +39,8 @@ key id_bo_phan(id_bo_phan),
 constraint id_vi_tri_fk foreign key (id_vi_tri) references vi_tri(id_vi_tri),
 constraint id_trinh_do_fk foreign key (id_trinh_do) references trinh_do(id_trinh_do),
 constraint id_bo_phan_fk foreign key (id_bo_phan) references bo_phan(id_bo_phan)
+on update cascade
+on delete cascade
 );
 drop table nhan_vien;
 
@@ -60,6 +62,8 @@ sdt varchar(45) not null,
 email varchar(45) not null,
 dia_chi varchar(45) not null,
 constraint id_loai_khach_fk foreign key (id_loai_khach) references loai_khach(id_loai_khach)
+on update cascade
+on delete cascade
 );
 drop table khach_hang;
 
@@ -83,6 +87,8 @@ key id_dich_vu_di_kem(id_dich_vu_di_kem),
 key id_hop_dong(id_hop_dong),
 constraint id_dich_vu_di_kem_fk foreign key (id_dich_vu_di_kem) references dich_vu_di_kem(id_dich_vu_di_kem),
 constraint id_hop_dong_fk foreign key (id_hop_dong) references hop_dong(id_hop_dong)
+on update cascade
+on delete cascade
 );
 drop table hop_dong_chi_tiet;
 
@@ -115,6 +121,8 @@ key id_kieu_thue(id_kieu_thue),
 key id_loai_dich_vu(id_loai_dich_vu),
 constraint id_kieu_thue_fk foreign key (id_kieu_thue) references kieu_thue(id_kieu_thue),
 constraint id_loai_dich_vu_fk foreign key (id_loai_dich_vu) references loai_dich_vu(id_loai_dich_vu)
+on update cascade
+on delete cascade
 );
 drop table dich_vu;
 
@@ -135,6 +143,8 @@ key id_dich_vu(id_dich_vu),
 constraint id_nhan_vien_hop_dong_fk foreign key (id_nhan_vien) references nhan_vien(id_nhan_vien),
 constraint id_khach_hang_hop_dong_fk foreign key (id_khach_hang) references khach_hang(id_khach_hang),
 constraint id_dich_vu_hop_dong_fk foreign key (id_dich_vu) references dich_vu(id_dich_vu)
+on update cascade
+on delete cascade
 );
 drop table hop_dong;
 
@@ -155,6 +165,8 @@ insert into bo_phan values (3,"nhan vien");
 insert into nhan_vien values (1,"Tran Van A",1,1,1,20011021,201817054,10000000,0905730724,"aaaa@gmail.com","so 1 duong A");
 insert into nhan_vien values (2,"K Van B",2,2,2,20011022,201817054,10000000,0905730724,"bbbb@gmail.com","so 1 duong B");
 insert into nhan_vien values (3,"Nguyen Van C",3,3,3,20011023,201817054,10000000,0905730724,"cccc@gmail.com","so 1 duong C");
+insert into nhan_vien values (4,"Le Van D",3,2,1,19911023,204817054,10000000,0906730724,"dddd@gmail.com","so 1 duong D");
+insert into nhan_vien values (5,"Tran Thi E",3,2,1,19941023,204617054,10000000,0906733724,"eeee@gmail.com","so 1 duong E");
 
 select * from vi_tri;
 select * from trinh_do;
@@ -207,10 +219,10 @@ insert into hop_dong values(2,2,2,2,20211124,20211126,1000000,500000000);
 insert into hop_dong values(3,3,3,3,20211125,20211127,1000000,200000000);
 insert into hop_dong values(4,1,4,3,20211126,20211128,1000000,200000000);
 insert into hop_dong values(5,1,4,2,20211128,20211129,1000000,200000000);
-insert into hop_dong values(10,1,5,4,20211129,20211130,1000000,200000000);
-insert into hop_dong values(11,2,6,1,20211130,20211203,1000000,200000000);
-insert into hop_dong values(12,3,7,2,20211201,20211205,1000000,200000000);
-
+insert into hop_dong values(10,1,5,4,20191129,20191130,1000000,200000000);
+insert into hop_dong values(11,2,6,1,20191130,20191203,1000000,200000000);
+insert into hop_dong values(12,3,7,2,20191201,20191205,1000000,200000000);
+ 
 insert into hop_dong_chi_tiet values(1,1,1,1);
 insert into hop_dong_chi_tiet values(2,2,2,2);
 insert into hop_dong_chi_tiet values(3,3,3,3);
@@ -243,7 +255,7 @@ group by khach_hang.ho_ten;
 -- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~  yêu cầu 4  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 select khach_hang.id_khach_hang, khach_hang.ho_ten, loai_khach.ten_loai_khach, hop_dong.id_hop_dong, dich_vu.ten_dich_vu,
-hop_dong.ngay_lam_hop_dong, hop_dong.ngay_ket_thuc, dich_vu.chi_phi_thue + (hop_dong_chi_tiet.so_luong * dich_vu_di_kem.gia) as TongTien
+hop_dong.ngay_lam_hop_dong, hop_dong.ngay_ket_thuc, dich_vu.chi_phi_thue + sum(hop_dong_chi_tiet.so_luong * dich_vu_di_kem.gia) as TongTien
 from khach_hang
 left join loai_khach on khach_hang.id_loai_khach = loai_khach.id_loai_khach 
 left join hop_dong on khach_hang.id_khach_hang = hop_dong.id_khach_hang
@@ -251,6 +263,7 @@ left join dich_vu on dich_vu.id_dich_vu = hop_dong.id_dich_vu
 left join hop_dong_chi_tiet on hop_dong.id_hop_dong = hop_dong_chi_tiet.id_hop_dong
 left join dich_vu_di_kem on dich_vu_di_kem.id_dich_vu_di_kem = hop_dong_chi_tiet.id_dich_vu_di_kem
 group by id_khach_hang;
+SET sql_mode=(SELECT REPLACE(@@sql_mode,'ONLY_FULL_GROUP_BY',''));
 
 -- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~  yêu cầu 5  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
@@ -294,17 +307,38 @@ having count(khach_hang.ho_ten) >= 1;
 
 -- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~  yêu cầu 8  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-select date_format(ngay_lam_hop_dong, "%m") as Thang, sum(tong_tien) as Tong_doanh_thu_thang, ngay_lam_hop_dong
+-- cách 1
+select date_format(ngay_lam_hop_dong, "%m") as Thang,count(hop_dong.id_hop_dong) as so_khach_hang_dang_ki, sum(hop_dong.tong_tien) as Tong_doanh_thu_thang
 from hop_dong
-group by month(ngay_lam_hop_dong)
-having year(hop_dong.ngay_lam_hop_dong) = "2019";
+where year(hop_dong.ngay_lam_hop_dong ) = 2019
+group by month(hop_dong.ngay_lam_hop_dong);
+
+-- cách 2	
+select temp.month, count(month(hop_dong.ngay_lam_hop_dong)) as so_khach_hang_dang_ki, sum(hop_dong.tong_tien) as Tong_tien from 
+(select 1 as month
+union select 2 as month
+union select 3 as month
+union select 4 as month
+union select 5 as month
+union select 6 as month
+union select 7 as month
+union select 8 as month
+union select 9 as month
+union select 10 as month
+union select 11 as month
+union select 12 as month) as temp
+left join hop_dong on month(hop_dong.ngay_lam_hop_dong) = temp.month
+left join khach_hang on khach_hang.id_khach_hang = hop_dong.id_khach_hang
+where year(hop_dong.ngay_lam_hop_dong) = "2019" or year(hop_dong.ngay_lam_hop_dong) is null or year(hop_dong.ngay_lam_hop_dong) is null
+group by temp.month
+order by temp.month;
 
 -- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~  yêu cầu 9  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 select hop_dong.id_hop_dong, hop_dong.ngay_lam_hop_dong, hop_dong.ngay_ket_thuc, hop_dong.tien_dat_coc, count(hop_dong_chi_tiet.id_hop_dong_chi_tiet) as So_luong_dich_vu_di_kem
 from hop_dong
 left join hop_dong_chi_tiet on hop_dong_chi_tiet.id_hop_dong = hop_dong.id_hop_dong
-group by hop_dong_chi_tiet.id_hop_dong_chi_tiet;
+group by hop_dong.id_hop_dong;
 
 -- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~  yêu cầu 10  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
@@ -318,25 +352,165 @@ where lk.ten_loai_khach = "Diamond" and (kh.dia_chi = "Quang Ngai" or kh.dia_chi
 
 -- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~  yêu cầu 11  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-select hd.id_hop_dong, nv.ho_ten as Ho_ten_nhan_vien, kh.ho_ten as Ho_ten_khach_hang, kh.sdt, dv.ten_dich_vu, sum(hdct.so_luong) as So_luong_dich_vu_di_kem, hd.tien_dat_coc
+select hd.id_hop_dong,hd.tong_tien, hd.tien_dat_coc, nv.ho_ten as Ho_ten_nhan_vien, kh.ho_ten as Ho_ten_khach_hang, kh.sdt, dv.ten_dich_vu,
+count(hdct.id_dich_vu_di_kem) as so_luong_dich_vu_di_kem from hop_dong hd
+inner join nhan_vien nv on hd.id_nhan_vien = nv.id_nhan_vien
+inner join khach_hang kh on hd.id_khach_hang = kh.id_khach_hang
+inner join dich_vu dv on hd.id_dich_vu = dv.id_dich_vu
+inner join hop_dong_chi_tiet hdct on hd.id_hop_dong = hdct.id_hop_dong
+where not exists(select hd.id_hop_dong where hd.ngay_lam_hop_dong between "20190101" and "20190630")
+and exists (select hd.id_hop_dong where hd.ngay_lam_hop_dong between "20191001" and "20191231");
+
+-- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~  yêu cầu 12  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ 
+
+select hdct.id_hop_dong_chi_tiet, hdct.id_dich_vu_di_kem, dvdk.ten_dich_vu_di_kem, hdct.so_luong
+from hop_dong_chi_tiet hdct
+left join dich_vu_di_kem dvdk on hdct.id_dich_vu_di_kem = dvdk.id_dich_vu_di_kem
+order by hdct.so_luong DESC;
+
+-- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~  yêu cầu 13  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ 
+
+select hd.id_hop_dong, dv.ten_dich_vu, dvdk.ten_dich_vu_di_kem, hdct.so_luong
 from hop_dong hd
-left join nhan_vien nv on hd.id_nhan_vien = nv.id_nhan_vien
-left join khach_hang kh on hd.id_khach_hang = kh.id_khach_hang
 left join dich_vu dv on hd.id_dich_vu = dv.id_dich_vu
 left join hop_dong_chi_tiet hdct on hd.id_hop_dong = hdct.id_hop_dong
-group by kh.ho_ten;
+left join dich_vu_di_kem dvdk on hdct.id_dich_vu_di_kem = dvdk.id_dich_vu_di_kem
+where hdct.so_luong = 1
+group by dvdk.ten_dich_vu_di_kem;
 
+-- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~  yêu cầu 14  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ 
 
+select hd.id_nhan_vien, nv.ho_ten, td.trinh_do, bp.ten_bo_phan, nv.sdt, nv.diachi, hd.id_hop_dong
+from hop_dong hd
+left join nhan_vien nv on hd.id_nhan_vien = nv.id_nhan_vien
+left join trinh_do td on nv.id_trinh_do = td.id_trinh_do 
+left join bo_phan bp on nv.id_bo_phan = bp.id_bo_phan
+group by nv.ho_ten;
 
-where (
-exists (select ngay_lam_hop_dong from hop_dong hd
-where date_format(hd.ngay_lam_hop_dong, "%Y %m %d") between "20191001" and "20191231")
-and not exists (select ngay_lam_hop_dong from hop_dong
-where date_format(hd.ngay_lam_hop_dong, "%Y %m %d") between "20190101" and "20190630")
+select hd.id_hop_dong, nv.ho_ten,td.trinh_do, bp.ten_bo_phan, nv.sdt, nv.diachi, count(hd.id_nhan_vien) as tong_so_hop_dong from hop_dong hd
+left join nhan_vien nv on hd.id_nhan_vien = nv.id_nhan_vien
+left join trinh_do td on nv.id_trinh_do = td.id_trinh_do 
+left join bo_phan bp on nv.id_bo_phan = bp.id_bo_phan
+where year(hd.ngay_lam_hop_dong) between "2018" and "2019"
+group by hd.id_nhan_vien
+having count(hd.id_nhan_vien) <= 3;
+
+-- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~  yêu cầu 15  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ 
+
+delete from nhan_vien where not exists (select nhan_vien.id_nhan_vien 
+from hop_dong where year(hop_dong.ngay_lam_hop_dong) between "2017" and "2021" 
+and hop_dong.id_nhan_vien = nhan_vien.id_nhan_vien);
+
+SET SQL_SAFE_UPDATES = 0; -- Handle Erorr 1175
+
+SET sql_mode=(SELECT REPLACE(@@sql_mode,'ONLY_FULL_GROUP_BY','')); -- Handle Error 1055
+-- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~  yêu cầu 16  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ 
+
+update khach_hang
+inner join hop_dong on hop_dong.id_khach_hang = khach_hang.id_khach_hang
+set khach_hang.id_loai_khach = 1
+where khach_hang.id_loai_khach = 2 and year(hop_dong.ngay_lam_hop_dong) = "2019"
+and exists (select sum(hop_dong.tong_tien) from hop_dong
+group by hop_dong.id_khach_hang
+having sum(hop_dong.tong_tien) > 10000000);
+
+-- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~  yêu cầu 17  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+SET FOREIGN_KEY_CHECKS=0;
+delete khach_hang from khach_hang
+inner join hop_dong on hop_dong.id_khach_hang = khach_hang.id_khach_hang 
+where exists (select hop_dong.ngay_lam_hop_dong from hop_dong
+where year(hop_dong.ngay_lam_hop_dong) <= "2016" and khach_hang.id_khach_hang = hop_dong.id_khach_hang)
+and not exists (select hop_dong.ngay_lam_hop_dong from hop_dong
+where year(hop_dong.ngay_lam_hop_dong) > "2016" and khach_hang.id_khach_hang = hop_dong.id_khach_hang);
+
+-- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~  yêu cầu 18  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+update dich_vu_di_kem dvdk
+inner join hop_dong_chi_tiet hdct on dvdk.id_dich_vu_di_kem = hdct.id_dich_vu_di_kem
+inner join hop_dong hd on hd.id_hop_dong = hdct.id_hop_dong
+set dvdk.gia = dvdk.gia * 2
+where year(hd.ngay_lam_hop_dong) = "2019" 
+and exists (select count(hop_dong_chi_tiet.id_dich_vu_di_kem) from hop_dong_chi_tiet
+group by hop_dong_chi_tiet.id_dich_vu_di_kem
+having count(hop_dong_chi_tiet.id_dich_vu_di_kem) > 10);
+ 
+-- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~  yêu cầu 19  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+select nv.id_nhan_vien, nv.ho_ten, nv.email, nv.sdt, nv.ngay_sinh, nv.diachi 
+from nhan_vien nv
+union
+select kh.id_khach_hang, kh.ho_ten, kh.email, kh.sdt, kh.ngay_sinh, kh.dia_chi
+from khach_hang kh;
+
+-- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~  yêu cầu 20  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+create view V_NHANVIEN
+as
+select nv.id_nhan_vien, nv.ho_ten, nv.id_vi_tri, nv.id_bo_phan, 
+nv.ngay_sinh, nv.so_cmnd, nv.luong,nv.sdt, nv.email,nv.diachi
+from nhan_vien nv
+inner join hop_dong hd on nv.id_nhan_vien = hd.id_nhan_vien
+where nv.diachi = "Hải Châu" and hd.ngay_lam_hop_dong = "20191212";
+
+drop view V_NHANVIEN;
+
+select * from V_NHANVIEN;
+
+-- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~  yêu cầu 21  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+update V_NHANVIEN
+set diachi = "Liên Chiểu";
+
+SET SQL_SAFE_UPDATES = 0;
+
+-- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~  yêu cầu 22  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+SET FOREIGN_KEY_CHECKS=0; -- set foreign key = 0 for deleting key row
+SET SQL_SAFE_UPDATES = 0;
+
+delimiter //
+create procedure sp_1 (in id_kh int)
+begin
+delete from khach_hang
+where khach_hang.id_khach_hang = id_kh;
+end//
+delimiter ;
+
+call sp_1(2);
+
+-- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~  yêu cầu 23  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+delimiter //
+create procedure sp_2 (in id_hd int,in id_nv int,in id_kh int,in id_dv int,in nlhd date, in nkt date, in tdc int, in tong int)
+begin 
+if(
+(id_hd not in (select hop_dong.id_hop_dong from hop_dong))
+and (id_nv in (select nhan_vien.id_nhan_vien from nhan_vien))
+and (id_kh in (select khach_hang.id_khach_hang from khach_hang))
+and (id_dv in (select dich_vu.id_dich_vu from dich_vu))
 )
+then insert into hop_dong value (id_hd, id_nv, id_kh, id_dv, nlhd, nkt, tdc, tong);
+end if;
+end //
+delimiter ;
 
+call sp_2(16,3,3,3,20140412,20140501,100000,200000000);
 
+-- ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~  yêu cầu 24  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
+delimiter $$
+create definer=current_user trigger tr_1 after delete on hop_dong
+for each row
+begin
+declare result int;
+select count(hop_dong.id_hop_dong) from hop_dong limit 1 into result;
+select * from hop_dong;
+end $$
+delimiter ;
 
+drop trigger tr_1;
+
+delete from hop_dong where hop_dong.id_hop_dong = 10;
 
 
